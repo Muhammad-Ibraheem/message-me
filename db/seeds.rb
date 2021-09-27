20b255639e4942723d6ec72678072d8ee97b1da4
Muhammad Ibraheem <ibrahimaziz719@gmail.com>
2021-09-27 13:20:09 +0500

    add users resource and mesasages resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "ibraheem", password: "password")
+User.create(username: "asaad", password: "password")
+User.create(username: "usman", password: "password")
+User.create(username: "bilal", password: "password")
+User.create(username: "aashir", password: "password")
+
+
+users = User.all
+
+Message.create(body: "Hi I am a new message", user: users.sample)
+Message.create(body: "Hi", user: users.sample)
+Message.create(body: "Hellow", user: users.sample)
+Message.create(body: "Hi how are you?", user: users.sample)
+Message.create(body: "good bye", user: users.sample)
