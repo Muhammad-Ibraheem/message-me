a99aac0573914d7e9a6306c7ecdd62f29a1ecf28
Muhammad Ibraheem <ibrahimaziz719@gmail.com>
2021-10-02 13:31:15 +0500

    add crerate custom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,10 +13,28 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+window.scrollBottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+
+window.submitMessage = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
+
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
-  $('.message .close')
-  .on('click', function() {
+  $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submitMessage();
+   scrollBottom();
 })
